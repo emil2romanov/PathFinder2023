41d2cb6a5befc78c47feab242f6f4202b9f7c9b0
Emil Romanov <eromanov8@gmail.com>
2023-10-05 15:18:45 +0300

    hashing the password

--- src/main/java/bf/softuni/pathfinder/service/impl/UserServiceImpl.java
+++ src/main/java/bf/softuni/pathfinder/service/impl/UserServiceImpl.java
@@ -5,24 +5,26 @@ import bf.softuni.pathfinder.model.dto.UserRegisterBindingModel;
 import bf.softuni.pathfinder.repository.UserRepository;
 import bf.softuni.pathfinder.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void register(UserRegisterBindingModel userRegisterBindingModel) {
         User user = modelMapper.map(userRegisterBindingModel, User.class);
-
+        user.setPassword(passwordEncoder.encode(userRegisterBindingModel.getPassword()));
         userRepository.save(user);
     }
 }
