39f38d73924a911214a7bf7835af536e61151475
Emil Romanov <eromanov8@gmail.com>
2023-10-06 09:33:56 +0300

    footer to fragment

---
+++ src/main/java/bf/softuni/pathfinder/web/AdminController.java
@@ -0,0 +1,14 @@
+package bf.softuni.pathfinder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public ModelAndView adminPanel() {
+        return new ModelAndView("/");
+    }
+}
