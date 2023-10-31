38e9bf7d1b20aa71e68b7e8b9a7acdadd9fb9ccb
Emil Romanov <eromanov8@gmail.com>
2023-10-31 22:54:01 +0200

    fixing RoutesController

--- src/main/java/bf/softuni/pathfinder/web/RoutesController.java
+++ src/main/java/bf/softuni/pathfinder/web/RoutesController.java
@@ -1,14 +1,22 @@
 package bf.softuni.pathfinder.web;
 
 import bf.softuni.pathfinder.model.dto.binding.AddRouteBindingModel;
+import bf.softuni.pathfinder.model.dto.view.RouteDetailsViewModel;
+import bf.softuni.pathfinder.model.dto.view.RouteViewModel;
 import bf.softuni.pathfinder.service.RouteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/routes")
