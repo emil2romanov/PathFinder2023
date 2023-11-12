84f812e96cbe8582492855027da15179a29d424c
Emil Romanov <eromanov8@gmail.com>
2023-11-12 15:01:05 +0200

    initial commit

--- src/main/java/bf/softuni/pathfinder/service/impl/RouteServiceImpl.java
+++ src/main/java/bf/softuni/pathfinder/service/impl/RouteServiceImpl.java
@@ -11,6 +11,10 @@ import bf.softuni.pathfinder.service.session.LoggedUser;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.UUID;
 import java.util.regex.Matcher;
@@ -19,7 +23,7 @@ import java.util.regex.Pattern;
 @Service
 public class RouteServiceImpl implements RouteService {
 
-    private static final String BASE_GPX_COORDINATES_PATH = ".\\src\\main\\java\\resources\\coordinates\\";
+    private static final String BASE_GPX_COORDINATES_PATH = ".\\src\\main\\resources\\coordinates\\";
 
     private final RouteRepository routeRepository;
     private final ModelMapper modelMapper;
@@ -46,6 +50,19 @@ public class RouteServiceImpl implements RouteService {
 
         String filePath = getFilePath(route.getName());
 
+        try {
+            File file = new File(BASE_GPX_COORDINATES_PATH + filePath);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+
+            OutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(addRouteBindingModel.getGpxCoordinates().getBytes());
+            route.setGpxCoordinates(filePath);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
         String regex = "v=(.*)";
         Pattern compile = Pattern.compile(regex);
 
@@ -60,15 +77,6 @@ public class RouteServiceImpl implements RouteService {
         routeRepository.save(route);
     }
 
-    private String getFilePath(String name) {
-        String pathPattern = "%s\\%s_%s.xml";
-
-        return String.format(
-                loggedUser.getUsername(),
-                name,
-                UUID.randomUUID());
-    }
-
     @Override
     public RouteDetailsViewModel getDetails(Long id) {
         Route route = routeRepository.findById(id)
@@ -78,5 +86,19 @@ public class RouteServiceImpl implements RouteService {
         routeDetailsViewModel.setAuthorName(route.getAuthor().getFullName());
 
         return routeDetailsViewModel;
+    }
+
+    private String getFilePath(String routeName) {
+        String pathPattern = "%s\\%s_%s.xml";
+
+        return String.format(pathPattern,
+                loggedUser.getUsername(),
+                transformRouteName(routeName),
+                UUID.randomUUID());
+    }
+
+    private String transformRouteName(String routeName) {
+        return routeName.toLowerCase()
+                .replaceAll("\\s+", "_");
     }
 }
